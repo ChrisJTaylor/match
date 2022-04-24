f25bb2a3ee80bde50e5b574f4e4ac83f01e8f125
Christian Taylor <christianjtaylor@sky.com>
2022-04-24 13:45:50 +0100

    tidy up invalid inputs

--- Match/Program.cs
+++ Match/Program.cs
@@ -2,31 +2,38 @@
 using Match.Domain;
 using SimpleInjector;
 
-namespace Match 
+namespace Match;
+
+[ExcludeFromCodeCoverage]
+internal class Program
 {
-    [ExcludeFromCodeCoverage]
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var container = SetupIoC();
+            
+        var playerInput = container.GetInstance<PlayerInput>();
+
+        try
         {
-            var container = SetupIoC();
-            
-            var playerInput = container.GetInstance<PlayerInput>();
-            
             var numberOfPacks = playerInput.AskPlayerHowManyPacksOfCardsToUse();
             Console.WriteLine();
             
             var matchingType = playerInput.AskPlayerWhichMatchingTypeToUse();
             Console.WriteLine();
         }
-
-        private static Container SetupIoC()
+        catch (InvalidInputException e)
         {
-            var container = new Container();
-            container.Options.ResolveUnregisteredConcreteTypes = true;
-            container.Register<IKeyboardInput, KeyboardInput>();
-            container.RegisterInstance<TextWriter>(Console.Out);
-            return container;
+            Console.WriteLine();
+            Console.WriteLine(e.Message);
         }
+    }
+
+    private static Container SetupIoC()
+    {
+        var container = new Container();
+        container.Options.ResolveUnregisteredConcreteTypes = true;
+        container.Register<IKeyboardInput, KeyboardInput>();
+        container.RegisterInstance<TextWriter>(Console.Out);
+        return container;
     }
 }
