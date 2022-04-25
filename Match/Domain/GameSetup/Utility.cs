f6e647aa486d8a61fc78687c2d65031611f7e7aa
Christian Taylor <christianjtaylor@sky.com>
2022-04-25 23:20:28 +0100

    remove the sleep as I no longer need this to sway the randomiser

--- Match/Domain/GameSetup/Utility.cs
+++ Match/Domain/GameSetup/Utility.cs
@@ -4,7 +4,6 @@ public static class Utility
 {
     public static Random GetRandomiser()
     {
-        Thread.Sleep(TimeSpan.FromMilliseconds(4));
         return new Random(DateTime.Now.Millisecond);
     }
 }
